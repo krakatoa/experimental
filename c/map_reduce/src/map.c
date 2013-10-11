45e7a82f45a810b85afbba6c0eb26d9f79fa5029
Fernando Alonso <krakatoa1987@gmail.com>
2013-10-11 01:01:01 -0300

    Making c/map_reduce threaded

--- c/map_reduce/src/map.c
+++ c/map_reduce/src/map.c
@@ -1,40 +1,74 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 
-void scanNext(char **word, FILE **file) {
+struct ThreadData {
+  int start, end;
+};
+
+void scanNext(char **word, FILE **file, int uid) {
   fscanf(*file, "%m[A-Za-z]%*[^$,. \t\v\f\n\r]", word);
   fgetc(*file);
   if (*word) {
-    printf("word: %s (at %d)\n", *word, ftell(*file));
+    //printf("(%d) -> %s (@%d)\n", uid, *word, ftell(*file));
+    printf("(%d) -> %s\n", uid, *word);
   }
 }
 
-int main(void) {
+void *map(struct ThreadData *td) {
+  //int r = rand() % 15;
+  //sleep(r);
+  int uid = rand() % 50 + 100;
+  //printf("Inside thread %d\n", uid);
+
+  printf("[%d][STARTED] %d-%d\n", uid, td->start, td-> end);
   FILE *fp;
   char *readWord;
-
-  int start = 0;
-  int end = 10;
 
   fp = fopen("input.txt", "r");
   if (fp == NULL) {
     printf("Could not open input file.\n");
-    return 1;
   } else {
-    fseek(fp, start, SEEK_SET);
-    if (start > 0) {
+    fseek(fp, td->start, SEEK_SET);
+    if (td->start > 0) {
       fscanf(fp, "%*[A-Za-z]%m[^$,. \t\v\f\n\r]", &readWord);
     }
-    while(!feof(fp) && ftell(fp) < end) {
-      scanNext(&readWord, &fp);
+    while(!feof(fp) && ftell(fp) < td->end) {
+      scanNext(&readWord, &fp, uid);
     }
-    if (ftell(fp) == end) { // It means last char is a space, so next batch will not know if string is truncated. Will process here
-      scanNext(&readWord, &fp);
+    if (ftell(fp) == td->end) { // It means last char is a space, so next batch will not know if string is truncated. Will process here
+      scanNext(&readWord, &fp, uid);
     }
     fclose(fp);
   }
   free(readWord);
+  printf("[%d][FINISHED]\n", uid);
+  
+  return NULL;
+}
+
+
+int main(void) {
+  pthread_t thread[4];
+  struct ThreadData data[4];
+  data[0].start = 0;
+  data[0].end = 9;
+  data[1].start = 10;
+  data[1].end = 19;
+  data[2].start = 20;
+  data[2].end = 29;
+  data[3].start = 30;
+  data[3].end = 39;
+
+  int i;
+  for (i = 0; i < 4; i++) {
+    pthread_create(&thread[i], NULL, (void *)map, &data[i]);
+  }
+  for (i = 0; i < 4; i++) {
+    pthread_join(thread[i], NULL);
+  }
+
 
   return 0;
 }
