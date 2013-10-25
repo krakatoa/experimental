d6c93579abbfc2d3242533afaa880e1a7cec1773
Fernando Alonso <krakatoa1987@gmail.com>
2013-10-25 08:18:09 -0300

    c/map_reduce fixed a bunch of memory leaks (on the map side)

--- c/map_reduce/src/map.c
+++ c/map_reduce/src/map.c
@@ -13,12 +13,40 @@ struct ReduceData {
   int *fd;
 };
 
-char *scanNext(char **word, FILE **file, int uid) {
-  fscanf(*file, "%m[A-Za-z]%*[^$,. \t\v\f\n\r]", word);
-  fgetc(*file);
-  if (*word) {
-    printf("(%d) -> %s (@%d)\n", uid, *word, ftell(*file));
-    return *word;
+void *skipFirst(FILE **file, int uid) {
+  char buffer[256];
+  int res;
+  res = fscanf(*file, "%*[A-Za-z]%[^$,. \t\v\f\n\r]", NULL, buffer);
+  
+  if (res) { 
+    fseek(*file, ftell(*file) - 1, SEEK_SET);
+  }
+}
+
+void *scanNext(char **word, FILE **file, int uid) {
+  char buffer[256];
+  int res;
+      //fscanf(*file, "%*[A-Za-z]%m[^$,. \t\v\f\n\r]", &readWord, NULL);
+  res = fscanf(*file, "%[A-Za-z]%*[^$,. \t\v\f\n\r]", buffer, NULL);
+  fseek(*file, ftell(*file) + 1, SEEK_SET);
+
+  //char *buffer;// = NULL;
+  //fscanf(*file, "%m[A-Za-z]%*[^$,. \t\v\f\n\r]", &buffer, NULL);
+  //fgetc(*file);
+
+  //char buffer[256];
+  //fgets(buffer, sizeof(buffer), *file);
+  //sscanf(buffer, "%[A-Za-z]%*[^$,. \t\v\f\n\r]", buffer, NULL);
+
+  if (res) {
+    //printf("(%d) -> %s (@%d)\n", uid, buffer, ftell(*file));
+    
+    //free(*word);
+    *word = (char *)malloc(strlen(buffer) + 1);
+    memcpy(*word, buffer, strlen(buffer) + 1);
+
+    //free(buffer);
+    //*word = buffer;
   }
 }
 
@@ -27,7 +55,7 @@ void *map(struct ThreadData *td) {
 
   //printf("[%d][STARTED] %d-%d\n", uid, td->start, td-> end);
   FILE *fp;
-  char *readWord;
+  char *readWord = NULL;
   char c;
 
   fp = fopen("input.txt", "r");
@@ -41,24 +69,15 @@ void *map(struct ThreadData *td) {
     if (isspace(c)) {
       printf("era un espacio nomas...\n");
     } else {
-      if (td->start > 0) {
-        fscanf(fp, "%*[A-Za-z]%m[^$,. \t\v\f\n\r]", &readWord);
-      }
+      if (td->start > 0) { skipFirst(&fp, uid); }
     }
     int result;
-    while(!feof(fp) && ftell(fp) <= td->end) {
+    while(!feof(fp) && ftell(fp) <= td->end + 1) {
       scanNext(&readWord, &fp, uid);
       if (readWord) {
+        printf("(%d) -> %s (@%d)\n", uid, readWord, ftell(fp));
         result = write(td->fd[1], readWord, (strlen(readWord) + 1));
-        //if (result != 1){
-        //  perror("write");
-        //}
-      }
-    }
-    if (ftell(fp) == td->end + 1) { // It means last char is a space, so next batch will not know if string is truncated. Will process here
-      scanNext(&readWord, &fp, uid);
-      if (readWord) {
-        result = write(td->fd[1], readWord, (strlen(readWord) + 1));
+        free(readWord);
         //if (result != 1){
         //  perror("write");
         //}
@@ -66,8 +85,8 @@ void *map(struct ThreadData *td) {
     }
     fclose(fp);
   }
-  free(readWord);
   //printf("[%d][FINISHED]\n", uid);
+  pthread_exit(NULL);
   
   return NULL;
 }
@@ -80,25 +99,27 @@ void *reduce(struct ReduceData *rd) {
 
   while(1) {
     result = read(rd->fd[0], buffer, sizeof(buffer));
+    //result = read(rd->fd[0], buffer, strlen(buffer) + 1);
     if (result < 1) {
       perror("read");
       exit(2);
     }
     printf("Read: %s\n", buffer);
-  } 
+  }
 
   printf("[FINISHED REDUCE]\n");
+  pthread_exit(NULL);
   return NULL;
 }
 
 int main(void) {
   pthread_t thread[4];
   struct ThreadData data[4];
-  
+
   int fd[2];
   pipe(fd);
-  int fd2[2];
-  pipe(fd2);
+  //int fd2[2];
+  //pipe(fd2);
   pthread_t reduce_t[1];
   struct ReduceData reduce_data[1];
   reduce_data[0].fd = fd;
@@ -109,15 +130,15 @@ int main(void) {
 
   data[1].start = 10;
   data[1].end = 19;
-  data[1].fd = fd2;
+  data[1].fd = fd;
 
   data[2].start = 20;
   data[2].end = 29;
-  data[2].fd = fd2;
+  data[2].fd = fd;
 
   data[3].start = 30;
   data[3].end = 39;
-  data[3].fd = fd2;
+  data[3].fd = fd;
 
   int i;
   pthread_create(&reduce_t[0], NULL, (void *)reduce, &reduce_data[0]);
@@ -129,7 +150,6 @@ int main(void) {
   for (i = 0; i < 4; i++) {
     pthread_join(thread[i], NULL);
   }
-  pthread_exit(NULL);
 
   return 0;
 }
